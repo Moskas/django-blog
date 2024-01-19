38db5e5a15263a0006e699e6afb0262ff86c6f5f
Moskas <minemoskas@gmail.com>
2024-01-19 23:41:03 +0100

    Basic backend with graphql

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Python dev flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python311
+          python311Packages.pyls-flake8
+          python311Packages.black
+          python311Packages.django
+          python311Packages.djangorestframework
+          python311Packages.graphene-django
+          python311Packages.django-cors-headers
+        ];
+      };
+    };
+}
