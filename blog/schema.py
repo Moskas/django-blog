e0b01496727ede492700dcef79c2a53820c796b8
Moskas <minemoskas@gmail.com>
2024-01-29 12:32:32 +0100

    Added latestPosts schema to graphql endpoint.
    Added latestPosts schema for getting 5 latest posts for the homepage.

--- blog/schema.py
+++ blog/schema.py
@@ -29,16 +29,15 @@ class Query(graphene.ObjectType):
     all_posts = graphene.List(PostType)
     all_tags = graphene.List(TagType)
     all_authors = graphene.List(AuthorType)
+    latest_posts = graphene.List(PostType)
     author_by_username = graphene.Field(AuthorType, username=graphene.String())
     post_by_slug = graphene.Field(PostType, slug=graphene.String())
     posts_by_author = graphene.List(PostType, username=graphene.String())
     posts_by_tag = graphene.List(PostType, tag=graphene.String())
     posts_by_id = graphene.List(PostType, id=graphene.ID())
 
-    def resolve_all_posts(root, info):
-        return (
-            models.Post.objects.prefetch_related("tags").select_related("author").all()
-        )
+    def resolve_all_posts(root, info, order_by=None):
+        return models.Post.objects.prefetch_related("tags").select_related("author").all()
 
     def resolve_all_tags(root, info):
         return models.Tag.objects.all()
@@ -74,6 +73,9 @@ class Query(graphene.ObjectType):
 
     def resolve_posts_by_id(root, info, id):
         return models.Post.objects.filter(id=id)
+
+    def resolve_latest_posts(root, info):
+        return models.Post.objects.prefetch_related("tags").select_related("author").order_by("-id")[:5]
 
 
 schema = graphene.Schema(query=Query)
